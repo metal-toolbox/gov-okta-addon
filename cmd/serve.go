45bc7810dc2e5bd4a539d41c6c8c82ac8b8bd8ec
E Camden Fisher <efisher@equinix.com>
2022-10-25 15:04:27 -0400

    Parallel NAT subscribers (#62)
    * add multiple subscribers for each queue group to ensure messages dont get accepted and then wait for processing

--- cmd/serve.go
+++ cmd/serve.go
@@ -20,6 +20,11 @@ import (
 	"golang.org/x/oauth2/clientcredentials"
 )
 
+const (
+	// defaultNATSQueueSize is the default for the number of subscribers per subject and queue group
+	defaultNATSQueueSize = 10
+)
+
 // serveCmd starts the gov-okta-addon service
 var serveCmd = &cobra.Command{
 	Use:   "serve",
@@ -50,6 +55,8 @@ func init() {
 	viperBindFlag("nats.subject-prefix", serveCmd.Flags().Lookup("nats-subject-prefix"))
 	serveCmd.Flags().String("nats-queue-group", "equinixmetal.governor.addons.gov-okta-addon", "queue group for load balancing messages across NATS consumers")
 	viperBindFlag("nats.queue-group", serveCmd.Flags().Lookup("nats-queue-group"))
+	serveCmd.Flags().Int("nats-queue-size", defaultNATSQueueSize, "queue size for load balancing messages across NATS consumers")
+	viperBindFlag("nats.queue-size", serveCmd.Flags().Lookup("nats-queue-size"))
 
 	// Tracing Flags
 	serveCmd.Flags().Bool("tracing", false, "enable tracing support")
@@ -129,7 +136,7 @@ func serve(cmdCtx context.Context, v *viper.Viper) error {
 		srv.WithNATSLogger(logger.Desugar()),
 		srv.WithNATSConn(nc),
 		srv.WithNATSPrefix(viper.GetString("nats.subject-prefix")),
-		srv.WithNATSQueueGroup(viper.GetString(("nats.queue-group"))),
+		srv.WithNATSQueueGroup(viper.GetString(("nats.queue-group")), viper.GetInt(("nats.queue-size"))),
 	)
 	if err != nil {
 		logger.Fatalw("failed creating new NATS client", "error", err)
